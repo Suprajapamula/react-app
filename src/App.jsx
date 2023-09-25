9d24b4da4f954d8bd7e278a5b01862c8d01893de
Suprajapamula <suprajapamula1449@gmail.com>
2023-09-25 17:26:20 +0530

    s

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Counter from './Counter';
+import './App.css';
+import Todolist from './Todolist';
+import { Outlet,Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='myBox'>
+         <h1>ReactJS application by Suprajapamula</h1>
+         <p>Hello edupoly</p>
+         <Link to="/counter">Counter</Link>&nbsp;
+         <Link to="/todolist">Todolist</Link>&nbsp;
+         <Outlet></Outlet>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Todolist from './Todolist'
+function Counter(){
+      return(
+        <div className='myBox'>
+            <h1>Welcome to Counter</h1>
+        </div>
+      )
+}
+export default Counter;
