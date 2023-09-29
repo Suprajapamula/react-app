6f3d331a2860c211f49211d5a640ba20bc6db393
Suprajapamula <suprajapamula1449@gmail.com>
2023-09-29 19:33:52 +0530

    DomBasedFormAndReactBasedForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import Counter from './Counter';
 import './App.css';
-import Todolist from './Todolist';
-import { Outlet,Link } from 'react-router-dom';
+import{ useState }from 'react';
 
-function App() {
-  return (
-    <div className='myBox'>
-         <h1>ReactJS application by Suprajapamula</h1>
-         <p>Hello edupoly</p>
-         <Link to="/counter">Counter</Link>&nbsp;
-         <Link to="/todolist">Todolist</Link>&nbsp;
-         <Outlet></Outlet>
-    </div>
-  );
+function App(){
+  function domBasedHandling(){
+        var x=document.getElementById("input1").value;
+        document.getElementById("headEl").innerHTML=x;
+  }
+  function reactBasedEventHandling(value){
+          setY(value);
+  }
+  var [y,setY]=useState("");
+         return (
+          <div>
+            <h1>Lets learn form handling in ReactJS</h1>
+            <label>This is DOM based Form Element</label>
+            <input type="text" onKeyUp={()=>{domBasedHandling()}} id="input1"/>
+            <h1 id="headEl">Your Entered text:</h1>
+
+            <hr/>
+
+            <label>This is ReactJS based Form Element</label>55
+            <input type="text" onKeyUp={(e)=>{reactBasedEventHandling(e.target.value)}}/>
+            <h1>Your Entered text:{y}</h1>
+          </div>
+         )
 }
 
 export default App;
